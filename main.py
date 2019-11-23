441a5a3b9c2ab872e6fca28ebf44df36c760b5b2
Aleksandar <sale.novakovic403@gmail.com>
2019-11-23 11:20:11 +0100

    starting window

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication
+import main_window
 
-app = QApplication(sys.argv)
-w = QWidget()
-w.show()
-sys.exit(app.exec_())
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    w = main_window.MainWindow()
+    sys.exit(app.exec_())
+
--- main_window.py
+++ main_window.py
@@ -1,1 +1,28 @@
 #Ovde ce biti kod za lavirint
+
+from PyQt5.QtWidgets import QGraphicsView, QLabel
+
+
+class MainWindow(QGraphicsView):
+
+    def __init__(self):
+        super().__init__()
+
+        self.width = 650
+        self.height = 500
+
+        self.score = 0
+        self.score_label = QLabel('Score: ', self)
+
+        self.init_ui()
+
+        self.show()
+
+    def init_ui(self):
+
+        self.setWindowTitle('Pac-Man')
+        self.setFixedWidth(self.width)
+        self.setFixedHeight(self.height)
+
+        self.score_label.move(5, 15)
+
