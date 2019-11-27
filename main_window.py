15314280518e2d611a67598929aff9cfda5c2d6c
aleksandar727 <alexandrr@gmail.com>
2019-11-27 21:04:53 +0100

    Dodato kretanje PacMan-a :)

--- main_window.py
+++ main_window.py
@@ -1,20 +1,25 @@
 #Ovde ce biti kod za lavirint
 
-from PyQt5.QtWidgets import QGraphicsView, QLabel
+from PyQt5.QtWidgets import QMainWindow, QLabel
+from PyQt5.QtGui import QPainter, QPen, QPixmap
+from PyQt5.QtCore import Qt
+import player
 
 
-class MainWindow(QGraphicsView):
+class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
 
-        self.width = 650
-        self.height = 500
+        self.width = 700
+        self.height = 650
+        self.label = QLabel(self)
 
         self.score = 0
         self.score_label = QLabel('Score: ', self)
 
         self.init_ui()
+        self.drawPlayer()
 
         self.show()
 
@@ -25,4 +30,36 @@ class MainWindow(QGraphicsView):
         self.setFixedHeight(self.height)
 
         self.score_label.move(5, 15)
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setPen(QPen(Qt.black, 5, Qt.SolidLine))
+        painter.drawRect(5, 40, self.width-10, self.height-50)
+
+    def drawPlayer(self):
+        self.pixmap = QPixmap("images/pacman_o.png")
+        self.label.setPixmap(self.pixmap)
+        self.label.move(200, 200)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Left:
+            self.movePlayerLeft(self.label)
+        elif event.key()  == Qt.Key_Right:
+            self.movePlayerRight(self.label)
+        elif event.key() == Qt.Key_Up:
+            self.movePlayerUp(self.label)
+        elif event.key() == Qt.Key_Down:
+            self.movePlayerDown(self.label)
+
+    def movePlayerLeft(self, label):
+        label.move(label.x()-5,label.y())
+
+    def movePlayerRight(self, label):
+        label.move(label.x()+5,label.y())
+
+    def movePlayerUp(self, label):
+        label.move(label.x(),label.y()-5)
+
+    def movePlayerDown(self, label):
+        label.move(label.x(),label.y()+5)
 
--- player.py
+++ player.py
@@ -1,1 +1,19 @@
 #kod za kretanje igraca, sakupljanje poena...
+
+from PyQt5.QtWidgets import QMainWindow, QLabel, QWidget, QFrame
+from PyQt5.QtGui import QPainter, QPen, QPixmap
+from PyQt5.QtCore import Qt
+import main_window
+
+class Player(main_window.QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+
+
+
+
+
+
+
