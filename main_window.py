3e636ac40a53192706a8476934371060f8881279
Aleksandar <sale.novakovic403@gmail.com>
2019-12-04 22:15:51 +0100

    poboljsano kretanje pacmana

--- main_window.py
+++ main_window.py
@@ -33,7 +33,7 @@ class MainWindow(QMainWindow):
         self.setWindowTitle('Pac-Man')
         self.setFixedWidth(self.width)
         self.setFixedHeight(self.height)
-        self.setWindowIcon(QIcon('images/player.png'))
+        self.setWindowIcon(QIcon('images/PacManRightEat.png'))
 
         self.center_window()
         self.setCentralWidget(self.map)
@@ -45,7 +45,7 @@ class MainWindow(QMainWindow):
         painter.drawRect(5, 40, self.width - 10, self.height - 50)
 
     def drawPlayer(self):
-        self.pixmap = QPixmap("images/pacman_o.png")
+        self.pixmap = QPixmap("images/PacManRightEat.png")
         self.label.setPixmap(self.pixmap)
         self.label.move(200, 200)
 
@@ -59,17 +59,45 @@ class MainWindow(QMainWindow):
         elif event.key() == Qt.Key_Down:
             self.movePlayerDown(self.label)
 
+    def keyReleaseEvent(self, event):
+        if event.key() == Qt.Key_Left:
+            self.set_left_close(self.label)
+        elif event.key() == Qt.Key_Right:
+            self.set_right_close(self.label)
+        elif event.key() == Qt.Key_Up:
+            self.set_up_close(self.label)
+        elif event.key() == Qt.Key_Down:
+            self.set_down_close(self.label)
+
+    #KRETANJE Pac Man-a
     def movePlayerLeft(self, label):
+        label.setPixmap(QPixmap("images/PacManLeftEat.png"))
         label.move(label.x() - 5, label.y())
 
     def movePlayerRight(self, label):
+        label.setPixmap(QPixmap("images/PacManRightEat.png"))
         label.move(label.x() + 5, label.y())
 
     def movePlayerUp(self, label):
+        label.setPixmap(QPixmap("images/PacManUpEat.png"))
         label.move(label.x(), label.y() - 5)
 
     def movePlayerDown(self, label):
+        label.setPixmap(QPixmap("images/PacManDownEat.png"))
         label.move(label.x(), label.y() + 5)
+
+    #funkcije za setovanje pixmape playera
+    def set_left_close(self, label):
+        label.setPixmap(QPixmap("images/PacManLeftClose.png"))
+
+    def set_right_close(self, label):
+        label.setPixmap(QPixmap("images/PacManRightClose.png"))
+
+    def set_up_close(self, label):
+        label.setPixmap(QPixmap("images/PacManUpClose.png"))
+
+    def set_down_close(self, label):
+        label.setPixmap(QPixmap("images/PacManDownClose.png"))
 
     """Center screen"""
 
@@ -91,27 +119,28 @@ class Board(QFrame):
 
         # 0 => zid    1=> tunel
         self.board = [
-             1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0,
-             1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0,
-             1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
-             1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0,
-             1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0,
-             0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-             0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0,
-             0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0,
-             0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0,
-             0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0,
-             1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0,
-             1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0,
-             1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0,
-             0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0,
-             1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0,
-             1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0,
+             [1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0],
+             [1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0],
+             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+             [1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0],
+             [1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0],
+             [0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0],
+             [0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0],
+             [0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0],
+             [0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0],
+             [0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0],
+             [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0],
+             [1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+             [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0],
+             [0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0],
+             [1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0],
+             [1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+
 
         ]
 
 
-    """
+
     def paintEvent(self, event):
         painter = QPainter(self)
         #ovde treba ispraviti
@@ -119,10 +148,10 @@ class Board(QFrame):
             for j in range(16):
                 if self.board[i][j] == 0:
                     #draw wall
-                    self.draw_map(i, j, painter, Qt.darkBlue)
+                    self.draw_map(i*60, j*60, painter, Qt.darkBlue)
                 else:
-                    self.draw_map(i, j, painter, Qt.black) #tunel
+                    self.draw_map(i*60, j*60, painter, Qt.black) #tunel
 
     def draw_map(self, x, y, painter, color):
+        #painter.fillRect(x, y, 60, 60, color)
         pass
-    """
--- player.py
+++ player.py
@@ -1,14 +1,5 @@
 #kod za kretanje igraca, sakupljanje poena...
 
-from PyQt5.QtWidgets import QMainWindow, QLabel, QWidget, QFrame
-from PyQt5.QtGui import QPainter, QPen, QPixmap
-from PyQt5.QtCore import Qt
-import main_window
-
-class Player():
-
-    def __init__(self):
-        super().__init__()
 
 
 
